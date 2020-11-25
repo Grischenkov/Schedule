c52fd2908bcc4753ebbb2b79206de4f734f43b62
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-25 21:18:56 +0300

    feat(model): добавить класс модели Lesson

---
+++ Schedule/Models/Lesson.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule.Models
+{
+    public class Lesson
+    {
+        public string Id { get; set; }
+
+        public string StartTime { get; set; }
+    }
+}
