59fce3cd87c4772f0cbd3801cfe0f5ae9604452e
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-25 21:19:54 +0300

    feat(model): добавить класс модели Teacher

---
+++ Schedule/Models/Teacher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Password { get; set; }
+    }
+}
