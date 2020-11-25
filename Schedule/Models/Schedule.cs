dce1df3f975e91b4382d291a6cd1c0534116a423
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-25 21:19:20 +0300

    feat(model): добавить класс модели Schedule

---
+++ Schedule/Models/Schedule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule.Models
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+
+        public string Subject { get; set; }
+
+        public int Period { get; set; }
+
+        public string StartDate { get; set; }
+
+        public string FinishDate { get; set; }
+
+        //Foreign keys
+        public int Group { get; set; }
+
+        public int Teacher { get; set; }
+
+        public string Lesson { get; set; }
+    }
+}
