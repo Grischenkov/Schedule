c10cad4ada098c4876757399e149e4f69fbd24bf
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-25 21:18:37 +0300

    feat(model): добавить класс модели Group

---
+++ Schedule/Models/Group.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+
+        public int Course { get; set; }
+
+        //Foreign keys
+        public int Faculty { get; set; }
+    }
+}
