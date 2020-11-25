a347bd035f475da0a167a43f7516ce66c221a318
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-25 21:21:11 +0300

    feat(model): добавить класс модели TeacherToDepartment

---
+++ Schedule/Models/TeacherToDepartment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule.Models
+{
+    public class TeacherToDepartment
+    {
+        //Foreign keys
+        public int Teacher { get; set; }
+
+        public int Department { get; set; }
+    }
+}
