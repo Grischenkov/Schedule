202e6e4bc714bb7368b2ba2aa08d2a63e597fcae
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-25 21:17:56 +0300

    feat(model): добавить класс модели Classroom

---
+++ Schedule/Models/Classroom.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule.Models
+{
+    public class Classroom
+    {
+        public string Id { get; set; }
+    }
+}
