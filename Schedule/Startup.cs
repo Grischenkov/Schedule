00f65c06a3afe35494b5cedfe40405cf9a127f68
Nikita Grishchenkov <nikita@grishchenkov.ru>
2020-11-26 13:59:18 +0300

    refactor: удалить автоматические сгенерированные файлы и библиотеки

--- Schedule/Startup.cs
+++ Schedule/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Schedule.ModelInterfaces;
+using Schedule.ModelMocks;
 
 namespace Schedule
 {
@@ -23,35 +25,25 @@ namespace Schedule
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddMvcCore();
+
+            services.AddTransient<IClass, MockClass>();
+            services.AddTransient<IClassroom, MockClassroom>();
+            services.AddTransient<IDay, MockDay>();
+            services.AddTransient<IFaculty, MockFaculty>();
+            services.AddTransient<IGroup, MockGroup>();
+            services.AddTransient<ILesson, MockLesson>();
+            services.AddTransient<ISchedule, MockSchedule>();
+            services.AddTransient<IStudent, MockStudent>();
+            services.AddTransient<ITeacher, MockTeacher>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
             app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-            });
         }
     }
 }
